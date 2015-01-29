c4a8d6a1d84f1edb46cca565d31b01d3ea97b770
Todd Gamblin <tgamblin@llnl.gov>
2015-01-29 11:33:13 -0800

    user2's change

--- hello.c
+++ hello.c
@@ -5,5 +5,8 @@ int main(int argc, char **argv) {
   printf("Hello world!\n");
   printf("Hello again world!\n");
   printf("done with issue53!\n");
+
+  printf("change from user2.\n");
+
   exit(0);
 }
